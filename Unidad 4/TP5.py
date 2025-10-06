6f51b141d96944acbe0dbe5413f7325fa54c1a7a
conijazmin90 <constanza.jimenez@tupad.utn.edu.ar>
2025-10-05 21:06:42 -0300

    Update TP5.py

--- Unidad 4/TP5.py
+++ Unidad 4/TP5.py
@@ -27,4 +27,31 @@ productosOrdenados = sorted(productos)
 print("\nLista de productos ordenados: ")
 print(productosOrdenados)
 
+#Ejercicio 3:
+numeros = [5, 8, 4, 10, 7, 3, 2, 20, 45, 65, 14, 4, 1, 89,99]
+
+num_pares = []
+num_impares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        num_pares.append(numero)
+    else:
+        num_impares.append(numero)
+
+print("Lista original de 15 números: ")
+print(numeros)
+
+print(f"Lista de números pares: ")
+print(num_pares)
+print(f"Cantidad de números pares: {len(num_pares)}")
+
+print(f"Lista de números impares: ")
+print(num_impares)
+print(f"Cantidad de números impares: {len(num_impares)}")
+
+#Ejercicio 4:
+datos = [1, 3, 5, 3, 7, 1, 9, 5, 3]
+
+
 
