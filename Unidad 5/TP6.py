b8b583c2a7e4345225c8e69e186675ccda6ffc13
conijazmin90 <constanza.jimenez@tupad.utn.edu.ar>
2025-10-13 18:39:00 -0300

    Update TP6.py

--- Unidad 5/TP6.py
+++ Unidad 5/TP6.py
@@ -1,6 +1,7 @@
-# Definición de funciones
-
-
-
-
-# Programa principal
+def modificar_lista(lista):
+    print("Dentro de la función antes de reasignar:", lista)
+    lista.append(4)
+    print("Dentro de la función después de reasignar:", lista)
+mi_lista = [1, 2, 3]
+modificar_lista(mi_lista)
+print("Fuera de la función:", mi_lista)
