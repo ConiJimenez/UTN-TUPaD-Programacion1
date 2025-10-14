bff34aae3cf9f3d1be4f57243f98e16c64c791b9
conijazmin90 <constanza.jimenez@tupad.utn.edu.ar>
2025-10-14 00:05:07 -0300

    Update TP6.py

--- Unidad 5/TP6.py
+++ Unidad 5/TP6.py
@@ -6,6 +6,12 @@ print(hola_mundo())
 
 # EJERCICIO N° 2:
 
+# Definición de funciones: 
+def saludar_usuario(nombre):
+    return saludar_usuario
 
+# Programa Principal:
+input("Ingrese su nombre: ")
+print(f"Hola {saludar_usuario}")
 
 
