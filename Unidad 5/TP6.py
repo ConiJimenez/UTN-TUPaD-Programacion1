fef3bddbc16aa772e432d83d2ed3a138f481302a
conijazmin90 <constanza.jimenez@tupad.utn.edu.ar>
2025-10-13 23:47:57 -0300

    Update TP6.py

--- Unidad 5/TP6.py
+++ Unidad 5/TP6.py
@@ -1,7 +1,11 @@
-def modificar_lista(lista):
-    print("Dentro de la función antes de reasignar:", lista)
-    lista.append(4)
-    print("Dentro de la función después de reasignar:", lista)
-mi_lista = [1, 2, 3]
-modificar_lista(mi_lista)
-print("Fuera de la función:", mi_lista)
+# EJERCICIO N° 1:
+def hola_mundo():
+    return "Hola Mundo!"
+
+print(hola_mundo())
+
+# EJERCICIO N° 2:
+
+
+
+
