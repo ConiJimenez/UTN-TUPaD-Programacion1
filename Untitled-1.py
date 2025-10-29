9bf34132f6851c2d6d51a7af5d30e80b4d7277b4
conijazmin90 <constanza.jimenez@tupad.utn.edu.ar>
2025-10-29 15:47:43 -0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,5 @@
-def calcular_precio_final(precio, descuento=0):
-    precio_final = precio - (precio * descuento / 100)
-    return round(precio_final, 2)
+precios = {"manzana": 1800, "naranja": 1500}
 
-resultado = calcular_precio_final(120, 15)
-print(resultado)
+precios["naranja"] = 1100
+
+print(precios)
