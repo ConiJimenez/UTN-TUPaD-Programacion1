38d4f2629eb3077ddefdee4792de039ee2e05a4c
conijazmin90 <constanza.jimenez@tupad.utn.edu.ar>
2025-11-01 17:45:08 -0300

    Update Parcial2.py

--- Parcial1.py/Parcial2.py
+++ Parcial1.py/Parcial2.py
@@ -35,4 +35,44 @@ def buscar_libro(catalogo, titulo):
 
 def mostrar_catalogo(catalogo):
     if len(catalogo) == 0:
-                
+        print("No hay libros cargados.")
+    else:
+        print("CATALOGO:")
+        for libro in catalogo:
+            print(libro["TITULO"], "-", libro["CANTIDAD"], "ejemplares")
+
+def listar_agotados(catalogo):
+    hay_agotados = False
+    for libro in catalogo:
+        if libro["CANTIDAD"] == 0:
+            print("-", libro["TITULO"])
+            hay_agotados= True
+        if not hay_agotados:
+            print("No hay libros agotados.") 
+
+def ingresar_titulos(catalogo):
+    cantidad = input("¿Cuantos libros querés cargar?")
+    if not cantidad.isdigit():
+        print("Debe ser un número.")  
+        return
+    cantidad = int(cantidad)
+    for _ in range(cantidad):
+        titulo = ""
+        while titulo == "":
+            titulo = input("Título del libro: ").strip()
+            if titulo == "":
+                print("No puede estar vacío")
+            elif buscar_libro(catalogo, titulo):
+                print("Ese libro ya existe")
+                titulo = ""
+        cant = input("Cantidad de ejemplares: ")
+        if not cant.isdigit():
+            print("Debe ser un número")
+            continue
+        cant = int(cant)
+        catalogo.append({"TITULO": titulo, "CANTIDAD": cant})
+        guardar_catalogo(catalogo)
+        print("Libro cargado correctamente.")
+
+
+    
